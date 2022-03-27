9f3213f27ea7ad20ff15821d80e636ba24760fb5
Elizabeth Rose Weeks <elizabethweeks@fb.com>
2022-03-27 16:07:12 -0700

    Revamp reviews and write real reviewsgit status

--- reviews/review.js
+++ reviews/review.js
@@ -4,71 +4,41 @@ class Review extends HTMLElement {
     }
 
   connectedCallback() {
-    var image_url = this.attributes.image_url.value
-    var website = this.attributes.website.value
-    var product_name = this.attributes.product_name.value
-    var left_arrow = this.attributes.left_arrow.value
-    var right_arrow = this.attributes.right_arrow.value
+    var website = this.attributes.website.value;
+    var product_name = this.attributes.product_name.value;
+    var review_text = this.attributes.review_text.value;
+    var discount_code = "";
+    var sponsored = this.attributes.sponsored.value;
+    var price = "";
+    var website_display = `<a href="${website}">${website}</a>`;
+    var rating = this.attributes.rating.value;
+    if (this.attributes.alternative_link !== undefined){
+      website_display = `<a href="${website}">${this.attributes.alternative_link.value}</a>`;
+    }
+    if (this.attributes.price !== undefined){
+      price = `<br> <strong class="review-title-text">Price: </strong>` + this.attributes.price.value;
+    }
+    if (this.attributes.discount_code !== undefined){
+      discount_code = `<br> <strong class="review-title-text">Discount Code: </strong>` + this.attributes.discount_code.value;
+    }
     this.innerHTML = `
-    <html>
-    <head>
-      <title>Daphne the Wiener</title>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <link rel="stylesheet" href="../css/style.css">
-      <link rel="stylesheet" href="../css/arrow.css">
-          <style>
-          /* First image (Logo. Full height) */
-      .bgimg-r {
-        background-image: url('../images/${image_url}');
-        min-height: 100%;
-        background-attachment: fixed;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: contain;
-      }
-      </style>
-          <div class="w3-top">
-          <div class="w3-bar" id="myNavbar">
-            <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
-              <i class="fa fa-bars"></i>
-            </a>
-            <a href="../index.html" class="w3-bar-item w3-button">HOME</a>
-            <a href="../index.html#about" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
-            <a href="../index.html#products" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i> PRODUCTS</a>
-            <a href="../index.html#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i> CONTACT</a>
-            <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-red">
-            </a>
-          </div>
-
-          <!-- Navbar on small screens -->
-          <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-            <a href="../index.html#about" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
-            <a href="../index.html#products" class="w3-bar-item w3-button" onclick="toggleFunction()">PRODUCTS</a>
-            <a href="../index.html#contact" class="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
+        <!-- Title section-->
+        <div class="title" id="title">
+          <img class="front-half" src="../images/front-half-stretched.png" alt="Italian Trulli">
+            <title-text><a href="${website}">${product_name}</a></title-text>
+          <img class="back-half" src="../images/back-half-stretched.png" alt="Italian Trulli">
+        </div>
+          <div class="review-section">
+            <div>
+              <p> <b class="review-title-text">Website: </b> ${website_display}
+              ${discount_code}<br>
+              <b class="review-title-text">Sponsored?: </b>${sponsored}
+              ${price}
+              <br> <b class="review-title-text">Rating: </b>${rating} out of 4 paws&#128062</p>
+            </div>
+              <p class="review-text">${review_text}</p>
           </div>
         </div>
-      </div>
-      <!-- First Parallax Image with Logo Text -->
-      <div class="bgimg-r w3-display-container w3-opacity-min">
-        <div class="w3-display-left w3-padding-large w3-pale-red w3-xlarge w3-wide w3-animate-opacity" style="white-space:nowrap;">
-          <button type="button" onclick="location.href='./${left_arrow}'"><<<</button>
-          <span class="w3-center">${product_name}</span>
-        </div>
-      </div>
-      <div class="w3-display-right w3-padding-large w3-pale-red" style="white-space:nowrap;">
-        <div>
-          <i class="fa fa-laptop fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i><a href="${website}">Website</a><br>
-          <i class="fa fa-paw fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>Promo: Daphne<br>
-          We loved this product 10/10! Daphine especially enjoyed making omlettes.
-        </div>
-        <button type="button" onclick="location.href='./${right_arrow}'">>>></button>
-        </div>
-      </body>
-    </html>
     `;
   }
 }
